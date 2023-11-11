c0dd1ad66beddee6878faa84aaa1cbd3500bfab6
Totto16 <tobiausgais@gmail.com>
2023-11-12 00:36:27 +0100

    add commander to parse arguments

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,80 @@
 import fs from "fs"
 import { programmParser } from "./parser"
 import { printError } from "./helpers"
+import { Command, CommandOptions, program } from "commander"
+const packageInfo = require("../package.json")
+
+interface TSCommand {
+	type: "ts"
+	output?: string
+}
+
+interface CppCommand {
+	type: "cpp"
+	output?: string
+}
+
+type ProgramCommand = CppCommand | TSCommand
+
+interface Options {
+	file: string
+	command: ProgramCommand
+}
+
+async function getOptions(): Promise<Options> {
+	return new Promise((resolve) => {
+		program
+			.name(packageInfo.name)
+			.description(packageInfo.description)
+			.version(packageInfo.version)
+
+		program
+			.command("cpp")
+			.description("generate a CPP header file")
+			.argument("<file>", "The file to process")
+			.option("-o, --output <string>", "the output file name")
+			.action(
+				(
+					file: string,
+					{ output }: { output?: string },
+					command: Command
+				) => {
+					resolve({
+						file,
+						command: { type: "cpp", output },
+					})
+				}
+			)
+
+		program
+			.command("ts")
+			.description("generate a ts type declaration file")
+			.argument("<file>", "The file to process")
+			.option("-o, --output <string>", "the output file name")
+			.action(
+				(
+					file: string,
+					{ output }: { output?: string },
+					command: Command
+				) => {
+					resolve({
+						file,
+						command: { type: "ts", output },
+					})
+				}
+			)
+
+		program.parse()
+	})
+}
 
 async function main() {
-	//TODO: use "commander" to parse args so that the file and mode (c++ or ts generator) can be set
+	const options = await getOptions()
 
-	if (process.argv.length != 3) {
-		console.error("Missing filename")
-		process.exit(1)
-	}
-
-	const file = process.argv[2]
+	const file = options.file
 
 	if (!fs.existsSync(file)) {
-		console.error(`File ${file} doesn't exist!`)
+		console.error(`File '${file}' doesn't exist!`)
 		process.exit(1)
 	}
 
@@ -24,10 +85,10 @@ async function main() {
 
 	if (parsed.isError) {
 		printError(file, parsed, fileData)
-		process.exit(1)
+		process.exit(2)
 	}
 
-	console.log(JSON.stringify(parsed.result, undefined, 4))
+	console.log(`Successfully parsed '${file}' file.`)
 
 	// typecheck
 
